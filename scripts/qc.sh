366f0e2aa9f23fdaf471e10e88a3ee3b9b27f0c6
Ram Vasuthevan <36580682+RamVasuthevan@users.noreply.github.com>
2023-04-19 18:40:29 -0400

    Quick

---
+++ scripts/qc.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Check if an argument is provided
+if [ $# -eq 0 ]; then
+    echo "Error: Please provide a branch name as an argument."
+    exit 1
+fi
+
+# Set variables
+arg1="$1"
+remote_branch="main"
+
+# Convert argument to lowercase and replace spaces with underscores
+new_branch_name=$(echo "$arg1" | tr '[:upper:]' '[:lower:]' | tr ' ' '_')
+
+# Run git commands
+git checkout $remote_branch
+git pull
+git checkout -b $new_branch_name
+git add .
+git commit -m "$arg1"
