72002b3ba8d3d518455dd21cbcb25bda5a22acfd
Ram Vasuthevan <36580682+RamVasuthevan@users.noreply.github.com>
2024-07-11 11:37:40 -0400

    Create Photo of the Day (#367)
    * Initial commit 🎉
    
    * Remove 2024-06-13
    
    * Add new
    
    * Correct date
    
    * Add 2024-06-15
    
    * Fix date
    
    * Add more
    
    * Add 2024-06-20
    
    * Add data for 2024-06-13
    
    * Add more details
    
    * Create generate-photo-of-the-day-metadata
    
    * Add more photos and details
    
    * Update generate-photo-of-the-day-metadata.py
    
    * Add more notes
    
    * Clean up
    
    * Clean up
    
    * Remove bullet point from h3

---
+++ scripts/generate-photo-of-the-day-metadata/generate-photo-of-the-day-metadata.py
@@ -0,0 +1,35 @@
+import os
+import yaml
+
+
+def generate_photo_of_the_day_metadata():
+    photo_metadata = {}
+    photo_directory = "/workspaces/Personal-Website/website/assets/photo-of-the-day"
+
+    for root, dirs, files in os.walk(photo_directory):
+        for filename in files:
+            if filename.endswith(".jpg") or filename.endswith(".jpeg"):
+                file_path = os.path.join(root, filename)
+                # Extract date from the file path
+                parts = file_path.split("/")
+                date = parts[-2]  # Assumes date is in second last part of path
+                image_name = filename
+
+                if date in photo_metadata:
+                    raise ValueError(f"Duplicate date found for {date}.")
+
+                # Assign to dictionary using date as key
+                photo_metadata[date] = {"display_image": image_name}
+
+    # Write metadata to YAML file
+    metadata_file = (
+        "/workspaces/Personal-Website/website/_data/photo-of-the-day-metadata.yml"
+    )
+    with open(metadata_file, "w") as file:
+        yaml.dump(photo_metadata, file, default_flow_style=False)
+
+    print(f"Generated metadata file: {metadata_file}")
+
+
+if __name__ == "__main__":
+    generate_photo_of_the_day_metadata()
