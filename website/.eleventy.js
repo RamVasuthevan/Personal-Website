3c5b7cd84306caaf5934212e6b455fd3455dd8c3
Ram Vasuthevan <36580682+RamVasuthevan@users.noreply.github.com>
2025-02-16 09:35:22 -0500

    Copied from https://github.com/RamVasuthevan/Private-Website/blob/579c9659191cd1e3ece2ef75c58dab4541fb86cd/website/.eleventy.js

---
+++ website/.eleventy.js
@@ -0,0 +1,71 @@
+export default function (eleventyConfig) {
+  eleventyConfig.ignores.add("_logs/**");
+
+  eleventyConfig.addLayoutAlias("post", "default.njk");
+  eleventyConfig.addLayoutAlias("page", "default.njk");
+
+  eleventyConfig.addPassthroughCopy("assets");
+
+  eleventyConfig.addPassthroughCopy("**/*.txt");
+  eleventyConfig.addPassthroughCopy("**/*.jpg");
+  eleventyConfig.addPassthroughCopy("**/*.png");
+
+  eleventyConfig.addPassthroughCopy("**/*.css");
+  eleventyConfig.addPassthroughCopy("**/*.js");
+
+  eleventyConfig.addPassthroughCopy("**/*.csv"); 
+
+  eleventyConfig.addPassthroughCopy("daily-journal/2025-02-11-daily-journal/voice-interface/");
+
+  eleventyConfig.setServerPassthroughCopyBehavior("passthrough");
+  
+  function generateImageHtml(params) {
+    const imageSrc = params.src 
+      ? params.src 
+      : `/assets/${params.directory || ''}/${params.name || ''}`;
+    
+    const captionHtml = params.caption
+      ? `<div class="caption">${params.caption}</div>`
+      : '';
+
+    return `
+      <div class="image-container">
+        <img src="${imageSrc}" 
+             alt="${params.alt || ''}" 
+             ${params.width ? `width="${params.width}"` : ''} 
+             ${params.height ? `height="${params.height}"` : ''}>
+        ${captionHtml}
+      </div>
+    `.trim();
+  }
+
+  eleventyConfig.addShortcode("image", function (params) {
+    return generateImageHtml(params);
+  });
+
+  eleventyConfig.addShortcode("unsplashImage", function (params) {
+    const unsplashCaption = `
+      Photo by <a href="https://unsplash.com/@${params.username}" target="_blank">${params.photographer}</a> 
+      from <a href="${params.image_link}" target="_blank">Unsplash</a>
+    `;
+
+    return generateImageHtml({
+      ...params,
+      caption: unsplashCaption,
+    });
+  });
+
+  return {
+    dir: {
+      input: ".", // Source folder
+      layouts: "_layouts",
+      data: "_data",
+      output: "_site", // Output folder
+    },
+    serverOptions: {
+      port: 4001,
+    },
+    htmlTemplateEngine: "njk",
+    markdownTemplateEngine: "njk",
+  };
+}
